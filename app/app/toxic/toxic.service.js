43eaa20dd3a41ea53c25fcfe72fc397f0c146fb5
Edu <e.marin.izquierdo@gmail.com>
2016-06-11 12:59:49 +0200

    redirection to https added

--- app/app/toxic/toxic.service.js
+++ app/app/toxic/toxic.service.js
@@ -6,18 +6,28 @@ angular.module('datafestApp')
         var _toxic = {};
 
         _toxic.dioxido = {
+            total: 1,
+            summ:0,
             value: 0
         };
         _toxic.monoxido = {
+            total: 1,
+            summ:0,
             value: 0
         };
         _toxic.dioxnit = {
+            total: 1,
+            summ:0,
             value: 0
         };
         _toxic.particulas = {
+            total: 1,
+            summ:0,
             value: 0
         };
         _toxic.ozono = {
+            total: 1,
+            summ:0,
             value: 0
         };
 
@@ -37,31 +47,41 @@ angular.module('datafestApp')
 
                         switch (data[i].parameter) {
                             case 1:
-                                _toxic.dioxido.value = data[i].value;
+                                _toxic.dioxido.summ += data[i].value;
+                                _toxic.dioxido.value = (_toxic.dioxido.summ / _toxic.dioxido.total);
+                                _toxic.dioxido.total++;
                                 _toxic.dioxido.color = (0 <= _toxic.dioxido.value <= 175) ? 
                                 '#4CAF50' : (176 <= _toxic.dioxido.value <= 350)?
                                 '#FFEB3B' : (351 <= _toxic.dioxido.value <= 525) ?
                                 '#FF9800' : '#F44336';
                             case 6:
-                                _toxic.monoxido.value = data[i].value;
+                                _toxic.monoxido.summ += data[i].value;
+                                _toxic.monoxido.value = (_toxic.monoxido.summ / _toxic.monoxido.total);
+                                _toxic.monoxido.total++;
                                 _toxic.monoxido.color = (0 <= _toxic.monoxido.value <= 5) ? 
                                 '#4CAF50' : (6 <= _toxic.monoxido.value <= 10)?
                                 '#FFEB3B' : (11 <= _toxic.monoxido.value <= 15) ?
                                 '#FF9800' : '#F44336';
                             case 8:
-                                _toxic.dioxnit.value = data[i].value;
+                                _toxic.dioxnit.summ += data[i].value;
+                                _toxic.dioxnit.value = (_toxic.dioxnit.summ / _toxic.dioxnit.total);
+                                _toxic.dioxnit.total++;
                                 _toxic.dioxnit.color = (0 <= _toxic.dioxnit.value <= 100) ? 
                                 '#4CAF50' : (101 <= _toxic.dioxnit.value <= 200)?
                                 '#FFEB3B' : (201 <= _toxic.dioxnit.value <= 300) ?
                                 '#FF9800' : '#F44336';
                             case 10:
-                                _toxic.particulas.value = data[i].value;
+                                _toxic.particulas.summ += data[i].value;
+                                _toxic.particulas.value = (_toxic.particulas.summ / _toxic.particulas.total);
+                                _toxic.particulas.total++;
                                 _toxic.particulas.color = (0 <= _toxic.particulas.value <= 50) ? 
                                 '#4CAF50' : (51 <= _toxic.particulas.value <= 90)?
                                 '#FFEB3B' : (91 <= _toxic.particulas.value <= 150) ?
                                 '#FF9800' : '#F44336';
                             case 14:
-                                _toxic.ozono.value = data[i].value;
+                                _toxic.ozono.summ += data[i].value;
+                                _toxic.ozono.value = (_toxic.ozono.summ / _toxic.ozono.total);
+                                _toxic.ozono.total++;
                                 _toxic.ozono.color = (0 <= _toxic.ozono.value <= 90) ? 
                                 '#4CAF50' : (91 <= _toxic.ozono.value <= 180)?
                                 '#FFEB3B' : (181 <= _toxic.ozono.value <= 240) ?
